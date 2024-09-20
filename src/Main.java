beb56cd95699c92bdda6269aa42c1ca4fe8c9ceb
fui96 <kidwelqn@mail.uc.edu>
2024-09-20 16:22:21 -0400

    innit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int RunningTotal = 267;
+        double SalesTax;
+        double Total;
+
+        SalesTax = RunningTotal * 0.05;
+        Total = SalesTax + RunningTotal;
+        System.out.println("Running Total:" + RunningTotal);
+        System.out.println("SalesTax: " + SalesTax);
+        System.out.println("Total: " + Total);
+        // I was having issues with the git repo that's why there is a lack of version history
+
+    }
+}
